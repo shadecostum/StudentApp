c528eb7242b3707db34d7ea7c6cfd4921bc68cbe
Akash santhosh <akash777kk@gmail.com>
2023-08-11 17:02:49 +0530

    studentApp

---
+++ Model/Student.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentApp.Model
+{
+    internal class Student
+    {
+        public int studentRollNoId;
+        public string studentName;
+        public double cGPA;
+        public string data;
+        
+
+        public Student(int studentRollNoId, string studentName, int cGPA)
+        {
+           
+            
+           //string erroe= ValidatingFunction(studentRollNoId, studentName, cGPA);
+            this.studentRollNoId=ValidateRollNo(studentRollNoId);
+            this.studentName = ValidateStudentName(studentName);
+            this.cGPA = ValidateCGPAe(cGPA);
+
+            
+           
+            
+        }
+
+        private double ValidateCGPAe(int cGPA)
+        {
+            if (cGPA > 10 || cGPA < 1)
+            {
+                Console.WriteLine("CGPA should be between 1 and 10.");
+                return 0.0; 
+            }
+            return (cGPA / 10.0) * 100.0;
+        }
+
+        private string ValidateStudentName(string studentName)
+        {
+            if (studentName.Length < 5)
+            {
+                Console.WriteLine("student name will be minimum 5 letters"); 
+                return "";
+            }
+            return studentName;
+        }
+
+        private int ValidateRollNo(int rollNo)
+        {
+           
+            if (rollNo < 1)
+            {
+                Console.WriteLine("student number will be positive");
+                return 0;
+            }
+            return  rollNo;
+            
+        }
+
+      /*  private string ValidatingFunction(int studentRollNoId, string studentName, int cGPA)
+        {
+            if (studentRollNoId < 1)
+            {
+                return "student number will be positive";
+            }
+            else if (studentName.Length < 5)
+            {
+                return "student name will be minimum 5 letters";
+            }
+            else if (cGPA > 10)
+            {
+                return "cgpa between 10";
+            }
+
+            return "Validation successful"; // This line is added for cases where no conditions are met.
+        }
+*/
+        public string ResultDetails()
+        {
+            return $" id : {studentRollNoId}, \n"+ $" Name : {studentName},\n" + $" percentage : {cGPA}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using StudentApp.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("------------------------------------");
+            Student studPerson=new Student(12,"akash",5);
+            Console.WriteLine(studPerson.ResultDetails());
+            Console.WriteLine("------------------------------------");
+            Student studPerson2 = new Student(-5, "aka", 45);
+            Console.WriteLine(studPerson2.ResultDetails());
+            Console.WriteLine("------------------------------------");
+            Student studPerson3 = new Student(8, "aka", 45);
+            Console.WriteLine(studPerson3.ResultDetails());
+            Console.WriteLine("------------------------------------");
+            Student studPerson4 = new Student(2, "akash", 45);
+            Console.WriteLine(studPerson4.ResultDetails());
+            Console.WriteLine("------------------------------------");
+            Student studPerson5 = new Student(3, "akash", 8);
+            Console.WriteLine(studPerson5.ResultDetails());
+            Console.WriteLine("------------------------------------");
+        }
+    }
+}
